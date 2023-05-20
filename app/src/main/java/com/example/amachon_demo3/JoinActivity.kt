e4208f2889c431fca11bae905a1a74d6faa832da
PARKJONGBEOM <jong980620@naver.com>
2023-05-20 20:47:24 +0900

    [FEAT] : 로그인 기능 구현

--- app/src/main/java/com/example/amachon_demo3/JoinActivity.kt
+++ app/src/main/java/com/example/amachon_demo3/JoinActivity.kt
@@ -3,7 +3,6 @@ package com.example.amachon_demo3
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
--- app/src/main/java/com/example/amachon_demo3/LogInActivity.kt
+++ app/src/main/java/com/example/amachon_demo3/LogInActivity.kt
@@ -3,8 +3,17 @@ package com.example.amachon_demo3
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.example.amachon_demo3.data.LoginDto
+import com.example.amachon_demo3.data.LoginResponseDto
+import com.example.amachon_demo3.network.Client
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +28,36 @@ class LogInActivity : AppCompatActivity() {
 
         val loginBtn = findViewById<Button>(R.id.loginBtn)
         loginBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            val email = findViewById<EditText>(R.id.email).text.toString()
+            val password = findViewById<EditText>(R.id.password).text.toString()
+            var isSuccess = false
+            var token = ""
+
+            Client.retrofitService.login(LoginDto(email, password)).enqueue(object : Callback<LoginResponseDto> {
+                override fun onResponse(
+                    call: Call<LoginResponseDto>,
+                    response: Response<LoginResponseDto>
+                ) {
+                    isSuccess = response.body()?.isSuccess!!
+                    if (isSuccess) {
+                        token = response.body()?.result?.accessToken!!
+                    }
+                }
+
+                override fun onFailure(call: Call<LoginResponseDto>, t: Throwable) {
+                }
+
+            })
+
+            Handler().postDelayed({
+                if (isSuccess) {
+                    Client.value = token
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                }
+            }, 1000)
         }
     }
 }
