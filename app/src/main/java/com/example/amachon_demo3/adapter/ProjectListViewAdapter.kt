ca1b3ec8fd64273f1cfc6fe5f062e7683a436cf1
devJSH1004 <devJSH1004@gmail.com>
2023-05-31 14:22:10 +0900

    [FEAT] Project search

---
+++ app/src/main/java/com/example/amachon_demo3/adapter/ProjectListViewAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.amachon_demo3.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.example.amachon_demo3.R
+import com.example.amachon_demo3.data.ProjectDto
+
+class ProjectListViewAdapter(val List : MutableList<ProjectDto>) : BaseAdapter() {
+    override fun getCount(): Int {
+        return List.size
+    }
+
+    override fun getItem(p0: Int): Any {
+        return List[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        var convertView = p1
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(p2?.context).inflate(R.layout.projectlistview_item, p2, false)
+        }
+
+        val mainTitle = convertView!!.findViewById<TextView>(R.id.projectMainTitle)
+        mainTitle.text = List[p0].title
+
+        /*
+        val subTitle = convertView!!.findViewById<TextView>(R.id.projectSubTitle)
+         */
+
+        return convertView!!
+    }
+
+}
